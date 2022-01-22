52031e9ea2aab611649d0bb3534169c8a94abbd8
paulinal3 <paulinal3@outlook.com>
2022-01-21 19:07:25 -0500

    creating hero component and rendering on App.js

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+
+export default function Hero() {
+    return (
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="subtitle">Hello, I'm</h1>
+            <h2 class="title">Paulina</h2>
+            <h1 class="subtitle profession">A Software Engineer</h1>
+          </div>
+        </div>
+    )
+}
