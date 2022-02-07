0ca0ad57d9dc24a3d2996e685b6faf417a2718e5
paulinal3 <paulinal3@outlook.com>
2022-02-06 22:42:15 -0500

    fixing deployed links bug

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -7,7 +7,7 @@ export default function Project(props) {
                 <span class="img_description is-overlay card-content">
                     <h1 class="is-uppercase">{props.project.name}</h1>
                     <p>{props.project.date} - {props.project.stackName}</p>
-                    <a href={props.project.repo} target='_blank' rel='noopener noreferrer'>GitHub Repository</a> | <a href="{props.project.deployedUrl}" target='_blank' rel='noopener noreferrer'>Deployed App</a>
+                    <a href={props.project.repo} target='_blank' rel='noopener noreferrer'>GitHub Repository</a> | <a href={props.project.deployedUrl} target='_blank' rel='noopener noreferrer'>Deployed App</a>
                     <p><i class="fas fa-info-circle" alt="Click for More Info"></i></p>
                 </span>
             </figure>
