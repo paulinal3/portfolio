d07e538a81622e7ceedf9159e9c08343a5b6e2f4
paulinal3 <paulinal3@outlook.com>
2022-03-01 18:02:45 -0500

    project items font and styling added

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -7,7 +7,10 @@ export default function Project(props) {
                 <span class="img_description is-overlay card-content">
                     <h1 class="is-uppercase">{props.project.name}</h1>
                     <p>{props.project.date} - {props.project.stackName}</p>
-                    <a href={props.project.repo} target='_blank' rel='noopener noreferrer'>GitHub Repository</a> | <a href={props.project.deployedUrl} target='_blank' rel='noopener noreferrer'>Deployed App</a>
+                    <div className="project-links">
+                        <a href={props.project.repo} target='_blank' rel='noopener noreferrer'>GitHub Repository</a>
+                        <a href={props.project.deployedUrl} target='_blank' rel='noopener noreferrer'>Deployed App</a>
+                    </div>
                     <p><i class="fas fa-info-circle" alt="Click for More Info"></i></p>
                 </span>
             </figure>
