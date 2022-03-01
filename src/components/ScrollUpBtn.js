04ac0fe8d29e3484a928f06df68e5af4f6413c6e
paulinal3 <paulinal3@outlook.com>
2022-03-01 10:37:54 -0500

    updating hero for phone size

--- src/components/ScrollUpBtn.js
+++ src/components/ScrollUpBtn.js
@@ -2,7 +2,7 @@
 export default function ScrollUpBtn() {
     return (
         <form action="#home">
-            <button id="toTop" title="Go to top">
+            <button id="to-top" title="Go to top">
                 <i class="fas fa-angle-up"></i>
             </button>
         </form>
